79f8b0f0a67405c6a2e6362da90fced3bc1c2dd8
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2022-06-30 15:20:02 -0500

    rt url resolver implemented and working

--- src/Components/DetailCards/SongDetailCard.js
+++ src/Components/DetailCards/SongDetailCard.js
@@ -1,13 +1,25 @@
 import parse from 'html-react-parser';
-import { createRichTextHtmlResolver, createAsyncRichTextHtmlResolver, linkedItemsHelper, contentItemResolver, contentItemHtml } from '@kentico/kontent-delivery';
+import { createRichTextHtmlResolver, createAsyncRichTextHtmlResolver, linkedItemsHelper, urlResolver, contentItemResolver, contentItemHtml } from '@kentico/kontent-delivery';
 import { nodeParser, asyncNodeParser } from '@kentico/kontent-delivery-node-parser';
 
 export default function SongDetailCard(props) {
     const projectId = process.env.REACT_APP_PROJECT_ID || '';
     const previewApiKey = process.env.REACT_APP_PREVIEW_API_KEY || '';
     const richTextElement = props.lyrics;
+    console.log(richTextElement)
     const resolvedRichText = createRichTextHtmlResolver().resolveRichText({
         element: richTextElement,
+        urlResolver: (linkId, linkText, link) => {
+            console.log(link)
+            let url = '#unsupported-link-type';
+            // Checks the content type of the linked content item
+            if (link.type === 'performer') {
+            url = `/performers/${link.codename}`;
+            }
+            return {
+              linkUrl: url,
+            };
+          },
         linkedItems: linkedItemsHelper.convertLinkedItemsToArray(richTextElement.linkedItems),
         contentItemResolver: (itemId, contentItem) =>{
             console.log(contentItem)
