76a37d3f85c80afb2fd8f97835bbd2171836e0fb
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2022-07-18 14:26:45 -0500

    localized album routing;

--- src/Pages/Albums.js
+++ src/Pages/Albums.js
@@ -2,10 +2,13 @@ import Card from "../Components/Card";
 import { GetItemsByType } from "../Utilities/Query";
 import { useEffect, useState  } from "react";
 import { SpinnerCircular } from 'spinners-react';
+import { useLang, useLangUpdate } from "../Utilities/GlobalContext";
+
 
 
 export default function Albums(props) {
-
+    const lang = useLang();
+    const updateLang = useLangUpdate();
     const [data, setData] = useState();
     const [isLoaded, setIsLoaded] = useState(false);
 
@@ -17,11 +20,57 @@ export default function Albums(props) {
             getData();
 
         },[]);
-        return(
-            <div>
-                {isLoaded? (data.data.items.map((item)=>
-                    <Card key = {item.system.codename} route={`/albums/${item.system.codename}`} title={item.elements.name.value} art={item.elements.artwork.value[0].url + '?w=100&h=100'}/>
-                )):(<SpinnerCircular />)}
-            </div>
-        )
+        switch (lang) {
+            case 'en':
+                return(
+                    <div>
+                        {isLoaded? (data.data.items.map((item)=>
+                            <Card key = {item.system.codename} route={`/albums/${item.system.codename}`} title={item.elements.name.value} art={item.elements.artwork.value[0].url + '?w=100&h=100'}/>
+                        )):(<SpinnerCircular />)}
+                    </div>
+                )        
+                break;
+            case 'es':
+                return(
+                    <div>
+                        {isLoaded? (data.data.items.map((item)=>
+                            <Card key = {item.system.codename} route={`/álbumes/${item.system.codename}`} title={item.elements.name.value} art={item.elements.artwork.value[0].url + '?w=100&h=100'}/>
+                        )):(<SpinnerCircular />)}
+                    </div>
+                )        
+                break;
+            case 'fr':
+                return(
+                    <div>
+                        {isLoaded? (data.data.items.map((item)=>
+                            <Card key = {item.system.codename} route={`/albums/${item.system.codename}`} title={item.elements.name.value} art={item.elements.artwork.value[0].url + '?w=100&h=100'}/>
+                        )):(<SpinnerCircular />)}
+                    </div>
+                )        
+                break;
+            case 'zh':
+                return(
+                    <div>
+                        {isLoaded? (data.data.items.map((item)=>
+                            <Card key = {item.system.codename} route={`/专辑/${item.system.codename}`} title={item.elements.name.value} art={item.elements.artwork.value[0].url + '?w=100&h=100'}/>
+                        )):(<SpinnerCircular />)}
+                    </div>
+                )        
+                break;
+            case 'ar':
+                return(
+                    <div>
+                        {isLoaded? (data.data.items.map((item)=>
+                            <Card key = {item.system.codename} route={`/ألبومات/${item.system.codename}`} title={item.elements.name.value} art={item.elements.artwork.value[0].url + '?w=100&h=100'}/>
+                        )):(<SpinnerCircular />)}
+                    </div>
+                )        
+                break;
+    
+    
+          
+    
+            default:
+                break;
+        }
 }
