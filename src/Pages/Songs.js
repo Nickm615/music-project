48a2a92e438aa268555e95ff2bbe8585a83e7abf
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2022-07-18 14:57:06 -0500

    songs localized

--- src/Pages/Songs.js
+++ src/Pages/Songs.js
@@ -2,9 +2,12 @@ import Card from "../Components/Card";
 import { GetItemsByType } from "../Utilities/Query";
 import { useEffect, useState  } from "react";
 import { SpinnerCircular } from 'spinners-react';
+import { useLang, useLangUpdate } from "../Utilities/GlobalContext";
 
 
 export default function Songs(props) {
+    const lang = useLang();
+    const updateLang = useLangUpdate();
     const [isLoaded, setisLoaded] = useState(false);
     const [data, setData] = useState();
         useEffect(() => {
@@ -14,14 +17,78 @@ export default function Songs(props) {
             };
             getData();
         },[]);
-        return(
-            <div>
+        switch (lang) {
+            case 'en':
+                return(
+                    <div>
+                        
+                        {isLoaded? (data.data.items.map((song)=>
+                        <Card key = {song.system.codename} route={`/songs/${song.system.codename}`} title={song.elements.name.value} 
+                        art={song.elements.album.linkedItems[0].elements.artwork.value[0].url + '?w=100&h=100'}
+                        />
+                        )):(<SpinnerCircular />)}
+                    </div>
+                )
+        
+                break;
+            case 'es':
+                return(
+                    <div>
+                        
+                        {isLoaded? (data.data.items.map((song)=>
+                        <Card key = {song.system.codename} route={`/canciones/${song.system.codename}`} title={song.elements.name.value} 
+                        art={song.elements.album.linkedItems[0].elements.artwork.value[0].url + '?w=100&h=100'}
+                        />
+                        )):(<SpinnerCircular />)}
+                    </div>
+                )
+        
+                break;
+            case 'fr':
+                return(
+                    <div>
+                        
+                        {isLoaded? (data.data.items.map((song)=>
+                        <Card key = {song.system.codename} route={`/chansons/${song.system.codename}`} title={song.elements.name.value} 
+                        art={song.elements.album.linkedItems[0].elements.artwork.value[0].url + '?w=100&h=100'}
+                        />
+                        )):(<SpinnerCircular />)}
+                    </div>
+                )
+            
+                    break;
+                case 'zh':
+                    return(
+                        <div>
+                            
+                            {isLoaded? (data.data.items.map((song)=>
+                            <Card key = {song.system.codename} route={`/歌曲/${song.system.codename}`} title={song.elements.name.value} 
+                            art={song.elements.album.linkedItems[0].elements.artwork.value[0].url + '?w=100&h=100'}
+                            />
+                            )):(<SpinnerCircular />)}
+                        </div>
+                    )
                 
-                {isLoaded? (data.data.items.map((song)=>
-                <Card key = {song.system.codename} route={`/songs/${song.system.codename}`} title={song.elements.name.value} 
-                art={song.elements.album.linkedItems[0].elements.artwork.value[0].url + '?w=100&h=100'}
-                />
-                )):(<SpinnerCircular />)}
-            </div>
-        )
+                        break;
+                    case 'ar':
+                        return(
+                            <div>
+                                
+                                {isLoaded? (data.data.items.map((song)=>
+                                <Card key = {song.system.codename} route={`/الأغاني/${song.system.codename}`} title={song.elements.name.value} 
+                                art={song.elements.album.linkedItems[0].elements.artwork.value[0].url + '?w=100&h=100'}
+                                />
+                                )):(<SpinnerCircular />)}
+                            </div>
+                        )
+                    
+                            break;
+        
+        
+    
+    
+        
+            default:
+                break;
+        }
 }
