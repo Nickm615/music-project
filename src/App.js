be3cd533c3855e71cec9469dc5bacbd247c4d2ac
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2022-08-03 10:14:37 -0500

    help

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import './App.css';
 import React, { useEffect } from 'react';
 import Navigation from './Components/Navigation';
 import { useLang } from './Utilities/GlobalContext';
+import KontentSmartLink, { KontentSmartLinkEvent } from '@kentico/kontent-smart-link';
+
 
 
 function App() {
   const lang = useLang();
 
-  useEffect(()=>{
-    document.body.setAttribute('data-kontent-project-id="b3e0a132-8366-0046-082c-844a2879e80d"',`data-kontent-language-codename="${lang}"`);
-  },[lang]);
+  useEffect(() => {
+    const kontentSmartLink = KontentSmartLink.initializeOnLoad({
+      defaultDataAttributes: {
+        projectId: "b3e0a132-8366-0046-082c-844a2879e80d",
+        languageCodename: "en",
+      },
+      queryParam: "preview-mode"
+    });
+    document.body.setAttribute("data-kontent-project-id", "b3e0a132-8366-0046-082c-844a2879e80d");
+    document.body.setAttribute("data-kontent-language-codename", `${lang}`)
+
+    return () => {
+      kontentSmartLink.destroy();
+    };
+  },[]);
+  // useEffect(()=>{
+  //   const plugin = KontentSmartLink.initialize({
+  //     queryParam: 'preview-mode',
+  //   });    
+  //   return () => {
+  //     plugin.destroy();
+  //   }
+  // },[lang]);
   return (
     <Navigation />
     );
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,10 +2,12 @@
 import { Link } from "react-router-dom";
 const pHolder = require("../assets/p-holder.jpg")
 
+
 export default function Card(props) {
+    console.log(props.id)
     return (
         <Link className='card-body' to={props.route}>
-            <div className= "card" data-kontent-item-id="${props.id}">
+            <div className= "card" data-kontent-item-id={props.id}>
                 <h2 className="card-heading">{props.title}</h2>
                 <img src= {props.art} alt = 'placeholder'/>
             </div>
